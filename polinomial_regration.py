6b43e236572ef94bddcafcf10409b5f6d98f8cde
Vinit-tala-777 <talavinit@gmail.com>
2026-04-09 16:05:51 -1200

    Update polinomial_regration.py

--- polinomial_regration.py
+++ polinomial_regration.py
@@ -3,17 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-# Example data
-x = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
-y = np.array([1, 4, 9, 16, 25])  # y = x^2
-
-# Create polynomial features (degree 2)
-poly = PolynomialFeatures(degree=2)
-x_poly = poly.fit_transform(x)
-
-# Fit the model
-model = LinearRegression()
-model.fit(x_poly, y)
 
 # Make predictions
 x_test = np.array([6, 7, 8]).reshape(-1, 1)
